d3152bd03420e94a252c49e74c7c4e0f1a2c25c5
Ivan <tol063115@mail.ru>
2020-01-12 20:26:41 +0300

    Ivan Ivanov, tol063115@mail.ru

--- school_papers/school_papers/apps/order/views.py
+++ school_papers/school_papers/apps/order/views.py
@@ -1,17 +1,11 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 from .forms import OrderForm
 from django.utils import timezone
+from docxtpl import DocxTemplate
 
 
 def homepage(request):
-    return render(request, 'order/main.html', {})
-
-
-def orderpage(request):
-    if request.method in ('GET', 'POST'):
-        send_mail('Subject of the Email', 'Работает!', 'tol063115@gmail.com', ['tol063115@gmail.com'])
     return render(request, 'order/main.html', {})
 
 
@@ -22,5 +16,14 @@ def new_order(request):
         if form.is_valid():
             editable = form.save(commit=False)
             editable.date = timezone.now()
+            doc = DocxTemplate("static/docs/document.docx")
+            form.save()
+            context = {'number': editable.id, 'surname': editable.surname, 'name': editable.name,
+                       'father': editable.father, 'class': editable.class_letter}
+            doc.render(context)
+            msg = EmailMessage('Справка', 'Справка с места учёбы', 'tol063115@gmail.com', ['editable.email'])
+            msg.content_subtype = "html"
+            msg.attach_file(doc)
+            msg.send()
             form.save()
     return render(request, 'order/new_order.html', {'form': form})
