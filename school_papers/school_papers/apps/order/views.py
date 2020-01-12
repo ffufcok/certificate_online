7bb038da9945267dd44cd13b0f445133a8c659aa
Ivan <tol063115@mail.ru>
2020-01-12 22:58:11 +0300

    Ivan Ivanov, tol063115@mail.ru

--- school_papers/school_papers/apps/order/views.py
+++ school_papers/school_papers/apps/order/views.py
@@ -16,12 +16,11 @@ def new_order(request):
         if form.is_valid():
             editable = form.save(commit=False)
             editable.date = timezone.now()
-            doc = DocxTemplate("static/docs/document.docx")
-            form.save()
+            doc = DocxTemplate('static/docs/d.docx')
             context = {'number': editable.id, 'surname': editable.surname, 'name': editable.name,
                        'father': editable.father, 'class': editable.class_letter}
             doc.render(context)
-            msg = EmailMessage('Справка', 'Справка с места учёбы', 'tol063115@gmail.com', ['editable.email'])
+            msg = EmailMessage('Справка', 'Справка с места учёбы', 'tol063115@gmail.com', [editable.email])
             msg.content_subtype = "html"
             msg.attach_file(doc)
             msg.send()
