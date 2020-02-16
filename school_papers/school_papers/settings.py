20701f9809c7ca1858f3b960c879ea90d816d569
Ivan <tol063115@mail.ru>
2020-02-16 23:12:59 +0300

    Ivan Ivanov, tol063115@mail.ru Password reset added!

--- school_papers/school_papers/settings.py
+++ school_papers/school_papers/settings.py
@@ -121,7 +121,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_URL = '/static/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
--- school_papers/school_papers/urls.py
+++ school_papers/school_papers/urls.py
@@ -11,6 +11,28 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='order/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='order/logout.html'), name='logout'),
     path('profile/', include('organisations.urls')),
+
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='order/password_reset.html'),
+         name='password_reset'),
+
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='order/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='order/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+
+    path('password-reset-complete',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='order/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
     path('', include('order.urls')),
 
 ]
